9c4d125e592a74706fd225223ddd4baaf8e2094b
asumanaslis <61350777+asumanaslis@users.noreply.github.com>
2022-06-06 23:02:48 +0300

    filter fix

--- filtermoviefrontend.php
+++ filtermoviefrontend.php
@@ -24,69 +24,6 @@
             <button>FILTER BY RATING</button>
         </form>
        
-
-        <br>
-        <br>
-        <br>
-        <form action="filtbyactor.php" method="POST">
-            <input type="text" name="leadingActor" placeholder="Actor Name"><br>
-        
-        
-            <br>
-            <button>FILTER BY LEADING ACTOR</button>
-        </form>
-        <br>
-        <br>
-        <br>
-        <form action="" method="post">
-            <select name="Leading Actor">
-                 <option value="">Select Leading Actor</option>
-                 <option value="55001">Matthew McConaughey</option>
-                 <option value="55002">Lindsay Lohan</option>
-                 <option value="55003">Ryan Gosling</option>
-                 <option value="55004">Christian Bale</option>
-                 <option value="55005">Justin Timberlake</option>
-                 <option value="55041">Zendaya</option>
-                 <option value="55038">Miles Teller</option>
-                 <option value="55043">Jennifer Lawrance</option>
-                 <option value="55034">Tom Hanks</option>
-            </select>
-        </form>
-
-        <br>
-        <br>
-        <br>
-    
-        
-
-        <form action="" method="post">
-            <select name="Director">
-                 <option value="">Select Director</option>
-                 <option value="82329">Damien Chazelle</option>
-                 <option value="83565">Denis Villeneuve</option>
-                 <option value="83456">Gary Ross</option>
-                 <option value="82001">Christopher Nolan</option>
-                 <option value="82003">Mark Waters</option>
-                 <option value="84007">Frank Darabont</option>
-                 <option value="85001">Andrew Niccol</option>
-               
-            </select>
-        </form>
-        <br>
-        <br>
-        <br>
-        
-
-        <form action="filtbygenre.php" method="POST">
-            <input type="text" name="genre" placeholder="Genre"><br>
-            
-            <br>
-            <button>FILTER BY GENRE</button>
-        </form>
-
-        <br>
-        <br>
-        <br>
         
         <form action="filtbyyear.php" method="POST">
             <input type="text" name="startYear" placeholder="Start Year"><br>
